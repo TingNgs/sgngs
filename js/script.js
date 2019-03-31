1e4a9ad52ed9f605727b997a5eeb888d7c3e2fcb
ngSgn <t105590049@ntut.org.tw>
2019-03-31 11:10:37 +0800

    check is mobile onclick card

--- js/script.js
+++ js/script.js
@@ -1,10 +1,30 @@
-function flipFrame() {
-  document.querySelector('#flipFrame').classList.toggle('hover');
-}
 $('.card-flip').click(function() {
-  console.log('yo');
-  $(this).toggleClass('hover');
-  setTimeout(() => {
-    $(this).removeClass('hover');
-  }, 2000);
+  if (isMobileBool) $(this).toggleClass('hover');
 });
+var isMobile = {
+  Android: function() {
+    return navigator.userAgent.match(/Android/i);
+  },
+  BlackBerry: function() {
+    return navigator.userAgent.match(/BlackBerry/i);
+  },
+  iOS: function() {
+    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  },
+  Opera: function() {
+    return navigator.userAgent.match(/Opera Mini/i);
+  },
+  Windows: function() {
+    return navigator.userAgent.match(/IEMobile/i);
+  },
+  any: function() {
+    return (
+      isMobile.Android() ||
+      isMobile.BlackBerry() ||
+      isMobile.iOS() ||
+      isMobile.Opera() ||
+      isMobile.Windows()
+    );
+  }
+};
+var isMobileBool = isMobile.any();
