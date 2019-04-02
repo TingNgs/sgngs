dfe9a134c56457375b6c713b8a2fbb8497b23bb4
ngSgn <t105590049@ntut.org.tw>
2019-04-02 20:46:04 +0800

    add mobile vh

--- js/nav.js
+++ js/nav.js
@@ -368,3 +368,11 @@ function mobileNavOnclick() {
   $('#mobile_nav_button').toggleClass('open');
   $('#mobile_nav').toggleClass('open');
 }
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
